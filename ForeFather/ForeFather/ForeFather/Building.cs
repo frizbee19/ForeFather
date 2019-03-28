34fdb1b403f7c5d6f1abb1298ad87abbb318dcd0
Ash <46610060+bawblez@users.noreply.github.com>
2019-03-28 14:29:50 -0500

    stuff

---
+++ ForeFather/ForeFather/ForeFather/Building.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForeFather
+    class Building : Tile
+    {
+        public Building() : base()
+        }
+
+        public Building(Texture2D s, Rectangle sR, Rectangle p) : base(s, sR, p)
+        {
+
+        }
+    }
+}
---
+++ ForeFather/ForeFather/ForeFather/Tile.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForeFather
+{
+    class Tile
+    {
+
+        private Texture2D spriteSheet;
+        private Rectangle sourceRect;
+        private Rectangle position;
+
+        public Tile()
+        {
+            
+        }
+
+        public Tile(Texture2D s, Rectangle sR)
+        {
+            spriteSheet = s;
+            sourceRect = sR;
+        }
+
+        public Tile(Texture2D s, Rectangle sR, Rectangle p)
+        {
+            spriteSheet = s;
+            sourceRect = sR;
+            position = p;
+        }
+
+        public Rectangle getPos()
+        {
+            return position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(spriteSheet, position, sourceRect, Color.White);
+        }
+
+    }
+}
