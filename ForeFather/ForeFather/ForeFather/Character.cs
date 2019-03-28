fa54ee85af1861a80f25fdc7bd56fb16f069cbe3
Humonculi5639 <humonculi5639@gmail.com>
2019-03-28 14:29:38 -0500

    abstract character class added

---
+++ ForeFather/ForeFather/ForeFather/Character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForeFather
+{
+    abstract class Character
+    {
+        private int maxhp;
+        private int offense;
+        private int defense;
+        private int luck;
+        private int mana;
+
+        public int getMaxHp { get; set; }
+        public int getOffense { get; set; }
+        public int getDefense { get; set; }
+        public int getLuck { get; set; }
+        public int getMana { get; set; }
+
+        public Character(int hp, int off, int def, int luck, int mana)
+        {
+            this.maxhp = hp;
+            this.offense = off;
+            this.defense = def;
+            this.luck = luck;
+            this.mana = mana;
+        }
+
+        abstract public void enterCombat();
+        abstract public void think(); //option selection in combat
+        abstract public void targeting(); //target selection
+        abstract public void attack(); //basic attack
+        abstract public void useMagic(); //takes a ability object as parameter
+        abstract public void useGoods(); //takes a inventory item as parameter
+        abstract public void runAway();
+        abstract public void faint();
+        abstract public void exitCombat();
+        
+
+    }
+}
