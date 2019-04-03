0d1173a24b312ce1deddbb918e3828350fbf2ee4
frizbee19 <frizbee19@gmail.com>
2019-04-03 14:10:10 -0500

    updated stuff idk

--- ForeFather/ForeFather/ForeFather/NPC.cs
+++ ForeFather/ForeFather/ForeFather/NPC.cs
@@ -26,11 +26,15 @@ namespace ForeFather
         }
         private Texture2D texture;
         private Rectangle location;
-        private List<string> lines;
         //the font needs to be monotype or whatever it is called so that it is easier to fit in the dialogue box
         private SpriteFont font;
         private ContentManager Content;
-        private int lineLength;
+        private List<TextBox> dialogue;
+        public TextBox Dialogue(int index)
+        {
+            return dialogue[index];
+        }
+
         public String Name
         {
             get { return name; }
@@ -40,11 +44,9 @@ namespace ForeFather
         public NPC(int x, int y, string n)
         {
             location = new Rectangle(x, y, 34, 50);
-            lines = new List<string>();
-            //placeholder length
-            lineLength = 30;
             name = n;
             Content.RootDirectory = "Content";
+            dialogue = new List<TextBox>();
             LoadContent();
         }
 
@@ -54,44 +56,49 @@ namespace ForeFather
 
         }
 
-        public void ReadFile(string path, int maxLength)
+        public void AddTextBox(string path)
         {
-            try
-            {
-                using (StreamReader reader = new StreamReader(path))
-                {
-                    while (!reader.EndOfStream)
-                    {
-                        //implement something that will prevent the line from extending the length
-                        string line = reader.ReadLine();
-                        //to separate into smaller lines
-                        List<string> tempLines = new List<string>();
-                        string[] words = line.Split(' ');
-                        lines.Add("");
-                        for(int i = 0; i < words.Length; i++)
-                        {
-                            if(tempLines[tempLines.Count - 1].Length + words[i].Length < maxLength)
-                            {
-                                tempLines[tempLines.Count - 1] += " " + words[i];
-                            }
-                            else
-                            {
-                                tempLines.Add(words[i]);
-                            }
-                        }
-                        
-                        for(int i = 0; i < tempLines.Count; i++)
-                        {
-                            lines.Add(tempLines[0]);
-                        }
-                    }
-                }
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine("lmao an error, you suck: " + e.Message);
-            }
+
         }
+
+        //public void ReadFile(string path, int maxLength)
+        //{
+        //    try
+        //    {
+        //        using (StreamReader reader = new StreamReader(path))
+        //        {
+        //            while (!reader.EndOfStream)
+        //            {
+        //                //implement something that will prevent the line from extending the length
+        //                string line = reader.ReadLine();
+        //                //to separate into smaller lines
+        //                List<string> tempLines = new List<string>();
+        //                string[] words = line.Split(' ');
+        //                lines.Add("");
+        //                for(int i = 0; i < words.Length; i++)
+        //                {
+        //                    if(tempLines[tempLines.Count - 1].Length + words[i].Length < maxLength)
+        //                    {
+        //                        tempLines[tempLines.Count - 1] += " " + words[i];
+        //                    }
+        //                    else
+        //                    {
+        //                        tempLines.Add(words[i]);
+        //                    }
+        //                }
+                        
+        //                for(int i = 0; i < tempLines.Count; i++)
+        //                {
+        //                    lines.Add(tempLines[0]);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch(Exception e)
+        //    {
+        //        Console.WriteLine("lmao an error, you suck: " + e.Message);
+        //    }
+        //}
 
         
 
--- ForeFather/ForeFather/ForeFather/TextBox.cs
+++ ForeFather/ForeFather/ForeFather/TextBox.cs
@@ -1,11 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
 
 namespace ForeFather
 {
     class TextBox
     {
+        private Rectangle box;
+        private Texture2D texture;
+        private const int DEFAULT_X = 50;
+        private const int DEFAULT_Y = 500;
+        private const int DEFAULT_WIDTH = 700;
+        private const int DEFAULT_HEIGHT = 200;
+        private List<string> lines;
+        private const int DEFAULT_LINELENGTH = 30;
+        private int lineLength;
+        private SpriteFont font;
+        private string path;
+        private int currentInd;
+        public int currentIndex
+        {
+            get { return currentInd; }
+        }
+
+        public TextBox(Texture2D t, Rectangle rect, int length, string p)
+        {
+            texture = t;
+            box = rect;
+            lineLength = length;
+            path = p;
+            ReadFile(@path);
+            lines = new List<string>();
+            currentInd = 1;
+        }
+
+        public TextBox(Texture2D t, int x, int y, int width, int height, int length, string p) : this(t, new Rectangle(x, y, width, height), length, p)
+        {
+
+        }
+
+        public TextBox(Texture2D t, int length, string p) : this(t, new Rectangle(DEFAULT_X, DEFAULT_Y, DEFAULT_WIDTH, DEFAULT_HEIGHT), length, p)
+        {
+
+        }
+
+        public TextBox(Texture2D t, Rectangle rect, string p) : this(t, rect, DEFAULT_LINELENGTH, p)
+        {
+
+        }
+
+        public TextBox(Texture2D t, int x, int y, int width, int height, string p) : this(t, new Rectangle(x, y, width, height), DEFAULT_LINELENGTH, p)
+        {
+
+        }
+
+        public TextBox(Texture2D t, string p) : this(t, new Rectangle(DEFAULT_X, DEFAULT_Y, DEFAULT_WIDTH, DEFAULT_HEIGHT), DEFAULT_LINELENGTH, p)
+        {
+
+        }
+
+        public void ReadFile(string path)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        //implement something that will prevent the line from extending the length
+                        string line = reader.ReadLine();
+                        //to separate into smaller lines
+                        List<string> tempLines = new List<string>();
+                        string[] words = line.Split(' ');
+                        lines.Add("");
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            if (tempLines[tempLines.Count - 1].Length + words[i].Length < lineLength)
+                            {
+                                tempLines[tempLines.Count - 1] += " " + words[i];
+                            }
+                            else
+                            {
+                                tempLines.Add(words[i]);
+                            }
+                        }
+
+                        for (int i = 0; i < tempLines.Count; i++)
+                        {
+                            lines.Add(tempLines[0]);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("lmao an error, you suck: " + e.Message);
+            }
+        }
+
+        public void scroll()
+        {
+            if (currentInd < lines.Count - 1)
+            {
+                currentInd++;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, box, Color.White);
+            spriteBatch.DrawString(font, lines[currentInd - 1], new Vector2(box.X + 30, box.Y + 30), Color.White);
+            spriteBatch.DrawString(font, lines[currentInd], new Vector2(box.X + 80, box.Y + 80), Color.White);
+        }
+
     }
 }
