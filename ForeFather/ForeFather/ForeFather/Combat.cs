98cc7c8f292ac1e14a0241f3f10bc2f585c8b65d
Humonculi5639 <humonculi5639@gmail.com>
2019-04-09 13:44:28 -0500

    Update Combat.cs

--- ForeFather/ForeFather/ForeFather/Combat.cs
+++ ForeFather/ForeFather/ForeFather/Combat.cs
@@ -15,73 +15,27 @@ namespace ForeFather
     {
         private List<Ally> allies;
         private List<Enemy> enemies;
-        private Rectangle selection;
-        private Texture2D selectionTex;
-        private int choice;
-        string combatText = "";
+        private bool turn;
+        Random rand = new Random();
 
-        public Combat(ContentManager content, List<Ally> allies, List<Enemy> enemies)
+        public Combat(List<Ally> allies, List<Enemy> enemies)
         {
-            selectionTex = content.Load<Texture2D>("blank");
-            selection = new Rectangle(0, 0, 28, 28);
-            allies[0].getTurn = true;
-            //enemies[0].getTurn = false;
             this.allies = allies;
             this.enemies = enemies;
+
+            if (rand.Next(1) == 0)
+                turn = false;
+            else
+                turn = true;
         }
-        
-        public void Draw(SpriteFont spriteFont, SpriteBatch spriteBatch)
+
+        public void update()
         {
-            if (allies[0].getTurn == true)
+            if (turn == true)
             {
-                spriteBatch.DrawString(spriteFont, "1. Bash \n2. Abilities \n3. Goods \n4. Run", new Vector2(0, 0), Color.White);
-                spriteBatch.Draw(selectionTex, selection, new Color(255, 0, 0, 75));
-                
+                for()
             }
-
-            DrawText(combatText, spriteFont, spriteBatch);
         }
 
-        public void DrawText(string text, SpriteFont spriteFont, SpriteBatch spriteBatch)
-        {
-            spriteBatch.DrawString(spriteFont, text, new Vector2(100, 200), Color.White);
-        }
-
-        public void makeChoice()
-        {
-
-        }
-
-        public void update(KeyboardState kb, KeyboardState oldkb)
-        {
-            if (allies[0].getTurn == true)
-            {
-                if (kb.IsKeyDown(Keys.Down) && !oldkb.IsKeyDown(Keys.Down))
-                {
-                    selection.Y += 26;
-                }
-                if (kb.IsKeyDown(Keys.Up) && !oldkb.IsKeyDown(Keys.Up))
-                {
-                    selection.Y -= 26;
-                }
-                if (kb.IsKeyDown(Keys.Enter) && !oldkb.IsKeyDown(Keys.Enter))
-                {
-                    if (selection.Y == 0)
-                    {
-                        allies[0].attack(enemies[0]);
-                        enemies[0].getTurn = true;
-                        combatText = "attack did " + 10 + " damage to enemy";
-                    }
-                }
-            }
-            if (enemies[0].getTurn == true)
-            {
-                enemies[0].getTurn = false;
-                allies[0].getTurn = true;
-            }
-
-
-        }
-      }
     }
-
+}
