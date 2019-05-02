108c80df8b8ec926cb515d00c4f7854d9361e916
frizbee19 <frizbee19@gmail.com>
2019-05-02 13:21:46 -0500

    idk

---
+++ ForeFather/ForeFather/ForeFather/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForeFather
+{
+    class Menu : TextBox
+    {
+        private List<TextBox> Menus;
+        private int index;
+    }
+}
