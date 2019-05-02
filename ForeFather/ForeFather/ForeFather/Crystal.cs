88d4796c56c3069f739424b6767f7905cc191c37
frizbee19 <frizbee19@gmail.com>
2019-05-02 14:00:00 -0500

    help

---
+++ ForeFather/ForeFather/ForeFather/Crystal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForeFather
+{
+    class Crystal : Item
+    {
+        private int mana;
+        public int Mana
+        {
+            get { return mana; }
+            set { mana = value; }
+        }
+
+        public Crystal(string n, string d, int magnitude) : base(n, d + "\r\nThe crystal stores " + magnitude + "extra mana")
+        {
+            mana = magnitude;
+        }
+
+        override
+        public void Use(Ally c)
+        {
+            c.getCrystal = this;
+        }
+
+        override
+        public bool Equals(Item item)
+        {
+            return item.Name == this.Name && ((Crystal)item).Mana == this.Mana && item.Description == this.Description;
+        }
+    }
+}
---
+++ ForeFather/ForeFather/ForeFather/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForeFather
+{
+    class Weapon : Item
+    {
+        private int damage;
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+
+        public Weapon(string n, string d, int magnitude) : base(n, d + "\r\nDeals " + magnitude + " damage")
+        {
+            damage = magnitude;
+        }
+
+        override
+        public void Use(Ally c)
+        {
+            c.getWeapon = this;
+        }
+
+        override
+        public bool Equals(Item item)
+        {
+            return item.Name == this.Name && ((Weapon)item).Damage == this.Damage && item.Description == this.Description;
+        }
+    }
+}
